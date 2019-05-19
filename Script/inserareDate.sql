820354c4bcfcf26accd66fab4ecba4dc73b283e0
MariaBulboaca <mariabulboaca@yahoo.com>
2019-05-19 17:32:06 +0300

    Insert suggestions

--- Script/inserareDate.sql
+++ Script/inserareDate.sql
@@ -25,3 +25,72 @@ VALUES ("Pantofi sport combinați","Pantofi sport cu construcţie tip şosetă. 
 
 INSERT INTO `recommendation`(`name`, `description`, `imagePath`, `gender`, `event`, `season`, `style`, `brand`, `color`, `trends`) 
 VALUES ("Balerini cu fundiță","Balerini cu bombeu contrastant şi fundiţă decorativă. Căptuşeală şi branţ din ţesătură.","imagini/Children/balerini.jpg","child","party","summer","modern","Zara","beige","modern");
+
+
+
+
+--de la Maria
+--women
+--timeToDance
+INSERT INTO suggestions (title,gender,description) VALUES('timeToDance','female','We want to look our best when we go clubbing and fabulous shoe styles will make you look good and feel great while dancing the night away. You may have to walk or run to the train to get there so you’ll be needing shoes that will let you experience the night without falling over or getting blisters. Keep scrolling on our list for stylish shoe style perfect for clubbing.If you’re not used to walking sky-high heels, you may go for kitten heeled shoes with 2 to 3 inches to give additional height to your stature while keeping you comfortable and giving you a great balance. You can have your pick from ankle strap sandals to peep toe shoes with kitten heels that can look great with ruffled dresses, asymmetrical dresses, shift dresses, pencil skirts and such. Kitten heels will still complement your outfit and show off your legs while keeping you secure on your feet.');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('timeToDance1','female','Kitten Heels','imagini/club1.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('timeToDance2','female','Wedges','imagini/club2.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('timeToDance3','female','Chunky Heels','imagini/club3.jpg');
+
+
+
+--timeToTravel
+
+INSERT INTO suggestions (title,gender,description) VALUES('timeToDance','female','Finding the right shoes for spring travel can be a bit confusing, especially when planning for cold and warm weather. Looking for the perfect travel shoe is sort of like shopping for a mattress. Everyone has different bodies, different preferences and finding the right balance for all is nearly imposible. However, one thing is for certain: travel shoes are not just about being functional but we`d love if they are also stylish, durable and versatile too. Here are three options to consider when packing shoes for spring travels.');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('timeToTravel1','female','Flats','imagini/flatss.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('timeToTravel2','female','Cotton shoes','imagini/coton.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('timeToTravel3','female','Boots','imagini/boots.jpg"');
+
+
+--timeToSummer
+
+INSERT INTO suggestions (title,gender,description) VALUES('summer','female','Summer is finally closer, so check out our Stylists`favorite warm weather outfit ideas! It`s time to break out the bright colors, bold patterns and strappy sandals.We`ve picked out how some of our favorite fashion girls have been rocking the strappy sandals, and after you see just how cool they make any outfit look, you`ll certainly want to purchase a pair (or two). ');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('summer1','female','Pair with a cute miniskirt for the ultimate `90s feel','imagini/outfit1.png');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('summer2','female','Socks and sandals? Counts us in','imagini/outfit2.png');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('summer3','female','A pair of strappy sandals will always look great with a maxi skirt','imagini/outfit3.png');
+
+
+--men
+
+--hunting 
+INSERT INTO suggestions (title,gender,description) VALUES('hunting','male','The hunting season is near, so these boots along with hunting costumes.');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('hunting2','male','Camo Shoes','imagini/hunn.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('hunting1','male','Jacket','imagini/hun.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('hunting3','male','Camo Set','imagini/hunting2.jpg');
+
+--summer
+INSERT INTO suggestions (title,gender,description) VALUES('summerM','male','It`s never too early to start thinking about fresh new shoes for summer. In fact, looking toward lighter, brighter footwear can help propel you out of any cold-weather trudge you might otherwise find yourself in.Crafting the perfect warm-weather collection of footwear is also easier when you get started ahead of the season—stocking up on sandals and low-top sneakers now means you`ll be ready to hit the ground running (actually) come summer. From waterproof slides to espadrilles, here are the best shoes to get you through next summer in style.');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('summerM1','male','<h2>Havaianas</h2>
+            <p>These come with a qualifier: They are to be worn at the beach or by
+            the pool only. But in those two locations, it`s hard to beat a good
+            pair of sandals.</p>
+          ','imagini/summerMen.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('summerM2','male','<h2>Vans</h2>
+          <p>
+            If you want to fully embrace that laid-back spring vibe, go with a
+            pair of sneakers worn by every California surfer and skater since
+            the dawn of time (or since Vans first started making them).
+          </p>','imagini/summerMen3.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('summerM3','male','  <h2>Birkenstock</h2>
+          <p>
+            More acceptable than flops: Birkenstocks. These are waterproof, so
+            they`re a sure bet around water.
+          </p>','imagini/summerMen2.jpg');
+
+		  
+		  
+--run
+INSERT INTO suggestions (title,gender,description) VALUES('sport','male','Whether you`re a routine marathon runner, or you simply
+        enjoy the occasional light jog, there`s no denying that a gorgeous,
+        sunny day can motivate just about anyone to get out there and pound the
+        pavement.');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('sport1','male','Just do it','imagini/sport.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('sport2','male','Nike Run','imagini/sport2.jpg');
+INSERT INTO suggestions(title,gender,description,imagePath) VALUES('sport3','male','Nike','imagini/sport3.jpg');
+		  
+		  
