2df9ec6cfa4ae0280ee9d9dfe27d49836d48f78b
MariaBulboaca <mariabulboaca@yahoo.com>
2019-05-21 19:29:12 +0300

    Register errors

--- phpfiles/register.php
+++ phpfiles/register.php
@@ -18,6 +18,7 @@
 			$repeatPassword=$_POST['repeatPass'];
 			$typeUser="user";
 			
+			echo $password,$repeatPassword;
 			
 			
 			if( $password != $repeatPassword && !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email))
@@ -40,26 +41,31 @@
 						header("location:../createAccount.php?InvalidPass=Incorrect password!Try again!");
 					}
 					else
-					if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email)){
-						setCookie('name',$name,time()+60);
-						setCookie('surname',$surname,time()+60);
-						setCookie('usernameR',$username,time()+60);
-						setCookie('country',$country,time()+60);
-						setCookie('pass',$password,time()+60);
-						setCookie('rePass',$repeatPassword,time()+60);
-						header("location:../createAccount.php?InvalidEmail=Invalid email!Try again!");
+						{
+							if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email)){
+								setCookie('name',$name,time()+60);
+								setCookie('surname',$surname,time()+60);
+								setCookie('usernameR',$username,time()+60);
+								setCookie('country',$country,time()+60);
+								setCookie('pass',$password,time()+60);
+								setCookie('rePass',$repeatPassword,time()+60);
+								header("location:../createAccount.php?InvalidEmail=Invalid email!Try again!");
+							}
+						
+							else
+							
+							{
+								$sql = "INSERT INTO users (username,password,firstname,lastname,typeUser,email,country,profileImage) VALUES ('".$username."','".$password."', '".$name."','".$surname."','".$typeUser."','".$email."','".$country."','".$profile."')";
+
+									if (mysqli_query($conn, $sql)) {
+										header("location:../login.php?LoginR=Registration complete! Login now");
+									} 
+
+									mysqli_close($conn);
+							}
 						}
-				}
-				$sql = "INSERT INTO users (username,password,firstname,lastname,typeUser,email,country,profileImage) VALUES ('".$username."','".$password."', '".$name."','".$surname."','".$typeUser."','".$email."','".$country."','".$profile."')";
-
-				if (mysqli_query($conn, $sql)) {
-					header("location:../login.php?LoginR=Registration complete! Login now");
-				} 
-
-				mysqli_close($conn);
-			
-		
-			
+					
+				}	
 		}
 	}
 	else
